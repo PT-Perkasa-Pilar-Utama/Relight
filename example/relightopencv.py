c5d81095859755e9a08f53cc877d20798b5a788e
mgi24 <ahmadmishbah@student.uns.ac.id>
2025-02-25 09:05:41 +0700

    'final'

--- example/relightopencv.py
+++ example/relightopencv.py
@@ -1,9 +1,7 @@
 import cv2
-from linear import linear_relight
-from skimage import exposure
+from relight import linear_relight
 
-
-image = cv2.imread("development/gelap.jpg")
+image = cv2.imread("yourimage.jpg")
 cv2.imshow('original', image)
 imageOut = linear_relight(image)
 cv2.imshow('linear', imageOut)
